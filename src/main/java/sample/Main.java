980d18d535f17ce2fb6d43aab2f6180b6b7944ce
tatianomnom <orangium@gmail.com>
2015-06-27 20:14:00 +0300

    gradle build

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("layout.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("layout.fxml"));
         primaryStage.setTitle("PlouPlay");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
